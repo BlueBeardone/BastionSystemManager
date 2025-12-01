bb5db2fb7a8ee70c613b0fec0b17a8ecb68654e0
BlueBeardone <barend.army@gmail.com>
2025-12-01 12:07:30 +0200

    Create ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+namespace Data;
+
+using Core.Models;
+using Core.Models.Enums;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Bastion> Bastions { get; set; }
+    public DbSet<Facility> Facilities { get; set; }
+    public DbSet<BasicFacility> BasicFacilities { get; set; }
+    public DbSet<SpecialFacility> SpecialFacilities { get; set; }
+    public DbSet<Hireling> Hirelings { get; set; }
+    public DbSet<BastionEvents> BastionEvents { get; set; }
+    public DbSet<Rewards> Rewards { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Configure TPH for Facility inheritance
+        builder.Entity<Facility>()
+            .HasDiscriminator<FacilityType>("FacilityType")
+            .HasValue<BasicFacility>(FacilityType.Basic)
+            .HasValue<SpecialFacility>(FacilityType.Special);
+            
+        // Configure relationships
+        builder.Entity<Bastion>()
+            .HasMany(b => b.Facilities)
+            .WithOne(f => f.Bastion)
+            .HasForeignKey(f => f.BastionId);
+            
+        builder.Entity<Facility>()
+            .HasMany(f => f.Hirelings)
+            .WithOne(h => h.Facility)
+            .HasForeignKey(h => h.FacilityId);
+    }
+}
