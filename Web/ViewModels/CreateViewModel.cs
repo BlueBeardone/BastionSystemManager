dd1609d360bc0786f1892a7af300c209b6059924
BlueBeardone <barend.army@gmail.com>
2025-12-01 13:13:58 +0200

    Create CreateViewModel.cs

---
+++ Web/ViewModels/CreateViewModel.cs
@@ -0,0 +1,13 @@
+namespace Web.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+
+public class CreateBastionViewModel
+{
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [StringLength(500)]
+    public string Description { get; set; }
+}
